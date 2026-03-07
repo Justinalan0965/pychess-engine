8c34c5fdd100f8301cfc4fa4c924daca57ff4337
Muthu Raj <mraj56965@gmail.com>
2026-03-08 01:34:57 +0530

    Used real pieces instead of self-explanatory pieces

--- Chess_Basic.py
+++ Chess_Basic.py
@@ -1,8 +1,9 @@
 import Rules
+from rich import print
 
 class Chess:
     def __init__(self):
-        self.board = self.create_board()
+        self.board = []
         self.isWhiteMove = True
         self.position_map = {
             'a': 0, 
@@ -20,45 +21,51 @@ class Chess:
         }
         self.rules = Rules.Rules(self)
 
+        self.chess_pieces = {
+            'R': '♖', 'N': '♘', 'B': '♗', 'Q': '♕', 'K': '♔', 'P': '♙',  # White
+            'r': '♜', 'n': '♞', 'b': '♝', 'q': '♛', 'k': '♚', 'p': '♟',  # Black
+            '.': '·'  # Empty square
+        }
+
     def create_board(self):
-        board = []
         for i in range(8):
             row = []
             for j in range(8):
-                row.append(' ')
-            board.append(row)
-        return board
+                row.append(self.chess_pieces['.'])
+            self.board.append(row)
+        return self.board
     
     def arrage_pieces(self):
-        self.board[0][0] = 'W-Rook'
-        self.board[0][1] = 'W-Knight'
-        self.board[0][2] = 'W-Bishop' 
-        self.board[0][3] = 'W-Queen'
-        self.board[0][4] = 'W-King'
-        self.board[0][5] = 'W-Bishop'
-        self.board[0][6] = 'W-Knight'
-        self.board[0][7] = 'W-Rook'
+        self.board[0][0] = self.chess_pieces['R']
+        self.board[0][1] = self.chess_pieces['N']
+        self.board[0][2] = self.chess_pieces['B']
+        self.board[0][3] = self.chess_pieces['Q']
+        self.board[0][4] = self.chess_pieces['K']
+        self.board[0][5] = self.chess_pieces['B']
+        self.board[0][6] = self.chess_pieces['N']
+        self.board[0][7] = self.chess_pieces['R']
         for i in range(8):
-            self.board[1][i] = 'W-Pawn'
+            self.board[1][i] = self.chess_pieces['P']
 
         for i in range(8):
-            self.board[6][i] = 'B-Pawn'
-        self.board[7][0] = 'B-Rook'
-        self.board[7][1] = 'B-Knight'
-        self.board[7][2] = 'B-Bishop'
-        self.board[7][3] = 'B-Queen'
-        self.board[7][4] = 'B-King'
-        self.board[7][5] = 'B-Bishop'
-        self.board[7][6] = 'B-Knight'
-        self.board[7][7] = 'B-Rook'
+            self.board[6][i] = self.chess_pieces['p']
+        self.board[7][0] = self.chess_pieces['r']
+        self.board[7][1] = self.chess_pieces['n']
+        self.board[7][2] = self.chess_pieces['b']
+        self.board[7][3] = self.chess_pieces['q']
+        self.board[7][4] = self.chess_pieces['k']
+        self.board[7][5] = self.chess_pieces['b']
+        self.board[7][6] = self.chess_pieces['n']
+        self.board[7][7] = self.chess_pieces['r']
 
     def move(self, crnt_pos, new_pos):
         row_from = int(crnt_pos[1]) - 1
         col_from = self.position_map[crnt_pos[0]]
         piece = self.board[row_from][col_from]
+        key = next((k for k, v in self.chess_pieces.items() if v == piece), None)
         
-        if (self.isWhiteMove and piece.startswith('W')) or (not self.isWhiteMove and piece.startswith('B')):
-            if self.rules.is_valid_move(crnt_pos, new_pos, piece) and not self.rules.is_piece_in_path(crnt_pos, new_pos):
+        if (self.isWhiteMove and key.isupper()) or (not self.isWhiteMove and key.islower()):
+            if self.rules.is_valid_move(crnt_pos, new_pos, key) and not self.rules.is_piece_in_path(crnt_pos, new_pos):
                 row_to = int(new_pos[1]) - 1
                 col_to = self.position_map[new_pos[0]]
                 self.board[row_to][col_to] = piece
@@ -66,9 +73,9 @@ class Chess:
                 self.isWhiteMove = not self.isWhiteMove
                 self.move_history["white" if not self.isWhiteMove else "black"].append((crnt_pos, new_pos))
             else:
-                print("Invalid move. Pls enter correct new position.")
+                print("[red]Invalid move. Pls enter correct new position.")
         else:
-            print(f"Invalid move. It's {'White' if self.isWhiteMove else 'Black'}'s turn.")
+            print(f"[red]Invalid move. It's [blue]{'White' if self.isWhiteMove else 'Black'}[red]'s turn.")
             
 
     def display_board(self):
@@ -81,11 +88,10 @@ class Chess:
         # Rotate board 180 degrees for black's perspective
         if not self.isWhiteMove:
             board_to_display = board_to_display[::-1]
-            # alpbt_pos = alpbt_pos[::-1]
             num_pos = num_pos[::-1]
 
         for i in range(7, -1, -1):
-            print(num_pos[i], end=' ')
+            print("[blue]" + num_pos[i], end=' ')
             for j in range(8):
                 piece = board_to_display[i][j]
                 print(piece.ljust(largest_piece_length), end=' ')   
@@ -93,12 +99,13 @@ class Chess:
 
         print("  ", end="")
         for letter in alpbt_pos:
-            print(letter.ljust(largest_piece_length), end=" ")
+            print("[blue]" + letter.ljust(largest_piece_length), end=" ")
         print()
 
         
 if __name__ == "__main__":
     chess = Chess()
+    chess.create_board()
     chess.arrage_pieces()
     chess.display_board()
 
--- Rules.py
+++ Rules.py
@@ -3,34 +3,36 @@ class Rules:
         self.chess = chess
     
     def is_valid_move(self, crnt_pos, new_pos, piece):
-        if piece == ' ':
+        if piece == '.':
             return False
-        piece_type = piece.split('-')[1]
-        if piece_type == 'Pawn':
+        piece_type = piece.upper()
+        if piece_type == 'P':
             return self.is_valid_pawn_move(crnt_pos, new_pos, piece)
-        if piece_type == 'Knight':
+        if piece_type == 'N':
             return self.is_valid_knight_move(crnt_pos, new_pos, piece)
-        if piece_type == 'Rook':
+        if piece_type == 'R':
             return self.is_valid_rook_move(crnt_pos, new_pos, piece)
-        if piece_type == 'Bishop':
+        if piece_type == 'B':
             return self.is_valid_bishop_move(crnt_pos, new_pos, piece)
-        if piece_type == 'Queen':
+        if piece_type == '':
             return self.is_valid_queen_move(crnt_pos, new_pos, piece)
+        if piece_type == 'K':
+            return self.is_valid_king_move(crnt_pos, new_pos, piece)
         return False
     
     def is_valid_pawn_move(self, crnt_pos, new_pos, piece):
-        direction = 1 if piece.startswith('W') else -1
-        start_row = 1 if piece.startswith('W') else 6
+        direction = 1 if piece.isupper() else -1
+        start_row = 1 if piece.isupper() else 6
         crnt_row = int(crnt_pos[1]) - 1
         crnt_col = self.chess.position_map[crnt_pos[0]]
         new_row = int(new_pos[1]) - 1
         new_col = self.chess.position_map[new_pos[0]]
         if crnt_col == new_col:
-            if new_row == crnt_row + direction and self.chess.board[new_row][new_col] == ' ':
+            if new_row == crnt_row + direction and self.chess.board[new_row][new_col] == '·':
                 return True
-            if crnt_row == start_row and new_row == crnt_row + 2 * direction and self.chess.board[crnt_row + direction][new_col] == ' ' and self.chess.board[new_row][new_col] == ' ':
+            if crnt_row == start_row and new_row == crnt_row + 2 * direction and self.chess.board[crnt_row + direction][new_col] == '·' and self.chess.board[new_row][new_col] == '·':
                 return True
-        elif abs(crnt_col - new_col) == 1 and new_row == crnt_row + direction and self.chess.board[new_row][new_col] != ' ' and not self.chess.board[new_row][new_col].startswith(piece[0]):
+        elif abs(crnt_col - new_col) == 1 and new_row == crnt_row + direction and self.chess.board[new_row][new_col] != '·' and not self.chess.board[new_row][new_col].startswith(piece[0]):
             return True
         return False
 
@@ -40,7 +42,7 @@ class Rules:
         new_row = int(new_pos[1]) - 1
         new_col = self.chess.position_map[new_pos[0]]
         if (abs(crnt_row - new_row) == 2 and abs(crnt_col - new_col) == 1) or (abs(crnt_row - new_row) == 1 and abs(crnt_col - new_col) == 2):
-            if self.chess.board[new_row][new_col] == ' ' or not self.chess.board[new_row][new_col].startswith(piece[0]):
+            if self.chess.board[new_row][new_col] == '·' or not self.chess.board[new_row][new_col].startswith(piece[0]):
                 return True
         return False
     
@@ -50,7 +52,7 @@ class Rules:
         new_row = int(new_pos[1]) - 1
         new_col = self.chess.position_map[new_pos[0]]
         if crnt_row == new_row or crnt_col == new_col:
-            if self.chess.board[new_row][new_col] == ' ' or not self.chess.board[new_row][new_col].startswith(piece[0]):
+            if self.chess.board[new_row][new_col] == '·' or not self.chess.board[new_row][new_col].startswith(piece[0]):
                 return True
         return False
     
@@ -60,7 +62,7 @@ class Rules:
         new_row = int(new_pos[1]) - 1
         new_col = self.chess.position_map[new_pos[0]]
         if abs(crnt_row - new_row) == abs(crnt_col - new_col):
-            if self.chess.board[new_row][new_col] == ' ' or not self.chess.board[new_row][new_col].startswith(piece[0]):
+            if self.chess.board[new_row][new_col] == '·' or not self.chess.board[new_row][new_col].startswith(piece[0]):
                 return True
         return False
     
@@ -73,7 +75,7 @@ class Rules:
         new_row = int(new_pos[1]) - 1
         new_col = self.chess.position_map[new_pos[0]]
         if abs(crnt_row - new_row) <= 1 and abs(crnt_col - new_col) <= 1:
-            if self.chess.board[new_row][new_col] == ' ' or not self.chess.board[new_row][new_col].startswith(piece[0]):
+            if self.chess.board[new_row][new_col] == '·' or not self.chess.board[new_row][new_col].startswith(piece[0]):
                 return True
         return False
 
@@ -86,17 +88,17 @@ class Rules:
         if crnt_row == new_row:
             step = 1 if new_col > crnt_col else -1
             for col in range(crnt_col + step, new_col, step):
-                if self.chess.board[crnt_row][col] != ' ':
+                if self.chess.board[crnt_row][col] != '·':
                     return True
         elif crnt_col == new_col:
             step = 1 if new_row > crnt_row else -1
             for row in range(crnt_row + step, new_row, step):
-                if self.chess.board[row][crnt_col] != ' ':
+                if self.chess.board[row][crnt_col] != '·':
                     return True
         elif abs(crnt_row - new_row) == abs(crnt_col - new_col):
             row_step = 1 if new_row > crnt_row else -1
             col_step = 1 if new_col > crnt_col else -1
             for i in range(1, abs(crnt_row - new_row)):
-                if self.chess.board[crnt_row + i * row_step][crnt_col + i * col_step] != ' ':
+                if self.chess.board[crnt_row + i * row_step][crnt_col + i * col_step] != '.':
                     return True
         return False
